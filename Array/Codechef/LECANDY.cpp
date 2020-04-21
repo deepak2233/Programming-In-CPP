a5bbb4613f02ce437c0d2783334f335a8fe8c776
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-04-21 13:19:06 +0530

    1.cpp

--- Array/Codechef/LECANDY.cpp
+++ Array/Codechef/LECANDY.cpp
@@ -1,1 +1,23 @@
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int tot_Elephant,tot_Candies,count=0;
+        cin>>tot_Elephant>>tot_Candies;
+        int arr[tot_Elephant];
+        for(int i=0;i<tot_Elephant;i++)
+        {
+            cin>>arr[i];
+            count+=arr[i];
+        }
+        if(tot_Candies>=count)
+            cout<<"Yes"<<endl;
+        else
+            cout<<"No"<<endl;
+    }
+}
