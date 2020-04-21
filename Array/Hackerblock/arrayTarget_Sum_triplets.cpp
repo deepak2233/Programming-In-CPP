b3086461811a56d75a16fbe802db679e7f627560
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-04-21 20:03:45 +0530

    Create arrayTarget_Sum_triplets.cpp

---
+++ Array/Hackerblock/arrayTarget_Sum_triplets.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+#include<algorithm>
+int main()
+{
+        int N,l,r,S;
+        cin>>N;
+        int arr[N];
+        for(int i=0;i<N;i++)
+        {
+            cin>>arr[i];
+        }
+        sort(arr,arr+N);
+        cin>>S;
+        for(int i=0;i<N-2;i++)
+        {
+            l=i+1;
+            r=N-1;
+            while(l<r)
+            {
+                if((arr[i]+arr[l]+arr[r]==S))
+                {
+                    cout<<arr[i]<<", "<<arr[l]<<" "<<"and"<<" "<<arr[r]<<endl;
+                    r--;
+                    l++;
+                }
+                else if((arr[i]+arr[l]+arr[r])>S)
+                {
+                    r--;
+                }
+                else if((arr[i]+arr[l]+arr[r]<S))
+                {
+                    l++;
+                }
+            }
+        }
+}
