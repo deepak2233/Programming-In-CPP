f26871c419fa9cd47e0a4d29226387e0c1524f71
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-04-22 13:45:17 +0530

    1

---
+++ Array/HackerRank/duplicateElement_array.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include<algorithm>
+
+int main() {
+
+
+		int n,res=0;
+        cin>>n;
+		int arr[n];
+		int arr2[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>arr[i];
+		}
+		for(int i=0;i<n;i++)
+		{
+			res^=arr[i];
+		}
+		for(int i=0;i<=n-2
+		;i++)
+		{
+			res^=i;
+		}
+		cout<<res<<"\n";
+
+
+}
+
+
