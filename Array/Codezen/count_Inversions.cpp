e55a3c7807c9d1b36e1cfd8e47965e52d418c418
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-04-22 16:32:25 +0530

    1

---
+++ Array/Codezen/count_Inversions.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include<algorithm>
+
+int main() {
+
+
+		int n,count=0;
+        cin>>n;
+		int arr[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>arr[i];
+		}
+		
+		for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    count++;
+                }
+            }
+        }
+        cout<<count<<"\n";
+}
+
+
