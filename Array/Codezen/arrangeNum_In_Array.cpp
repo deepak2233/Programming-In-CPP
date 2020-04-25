9708f589804a53c9917c5f7b3a23388db9547c85
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-04-25 17:30:08 +0530

    1

---
+++ Array/Codezen/arrangeNum_In_Array.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void arrange(int arr[], int n) 
+{
+    int left = 0;
+    int right = n - 1;
+    int counter = 1;
+    while(left <= right) 
+    {
+        if(counter % 2 == 1) 
+        {
+            arr[left] = counter;
+            counter++;
+            left++;
+        }
+        else
+        {
+            arr[right] = counter;
+            counter++;
+            right--;
+        }
+    }
+}
+
+int main(){
+  int n;  
+  cin >> n;
+  
+  int* arr = new int[n];
+  arrange(arr,n);
+  for(int i = 0; i < n; i++){
+    cout << arr[i] << " ";
+  }
+  delete arr;
+}
