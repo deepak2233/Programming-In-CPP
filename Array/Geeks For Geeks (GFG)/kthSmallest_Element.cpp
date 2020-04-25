f7726eea77d4254006f4582b4a685b75ea8c81bc
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-04-25 19:01:39 +0530

    1.cpp

---
+++ Array/Geeks For Geeks (GFG)/kthSmallest_Element.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,k;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>arr[i];
+	    }
+	    cin>>k;
+	    sort(arr,arr+n,greater<int>());
+	    for(int i=n;i>=0;i--)
+	    {
+	        cout<<arr[n-k]<<"\n";
+	        break;
+	    }
+
+
+	}
+	return 0;
+}
