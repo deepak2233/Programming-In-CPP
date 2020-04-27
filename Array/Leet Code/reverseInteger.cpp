7ff3db4d0e604a254212bc4af3bc29ece5f09282
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-04-27 20:11:29 +0530

    1

---
+++ Array/Leet Code/reverseInteger.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+    if(x<10 && x>=0) 
+        return x;
+   long long res = 0;
+   while(x)
+   {
+     res = res*10 + x%10;
+     x /= 10;
+    }
+   return (res<INT_MIN || res>INT_MAX) ? 0 : res;
+ }
+};
