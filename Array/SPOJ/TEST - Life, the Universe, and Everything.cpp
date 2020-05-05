57c154ffeff264c224391769605bcade5698d97a
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-05-05 16:17:22 +0530

    1

---
+++ Array/SPOJ/TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	while(cin>>n)
+    {
+        if(n==42)
+         {
+             break;
+         }
+        cout<<n<<"\n";
+    }
+	return 0;
+}
