ee5496c51da040c56ce3b56a720696b36372d432
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-05-17 19:39:06 +0530

    1

---
+++ Array/PrepBytes/1) smaller_Greater_Equal_Numbers.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+#ifndef ONLINE_JUDGE 
+ 	freopen("input.text", "r", stdin); 
+ 	freopen("output.text", "w", stdout); 
+#endif
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n,K,count_L=0,count_E=0,count_M=0;
+    	cin>>n>>K;
+    	int arr[n];
+    	for(int i=0;i<n;i++)
+    	{
+    		cin>>arr[i];
+    	}
+    	for(int i=0;i<n;i++)
+    	{
+    		if(arr[i]<K)
+    		{
+    			count_L++;
+    		}
+    		if(arr[i]>K)
+    		{
+    			count_M++;
+    		}
+    		if(arr[i]==K)
+    		{
+    			count_E++;
+    		}
+    	}
+    	cout<<count_L<<" "<<count_M<<" "<<count_E<<"\n";
+    }
+}
+    
