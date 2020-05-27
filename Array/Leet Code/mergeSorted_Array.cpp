49d84d16d181b180199c088016686e84ee4a1679
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-05-27 11:37:23 +0530

    Delete mergeSorted_Array.cpp

--- Array/Leet Code/mergeSorted_Array.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    std::copy(nums2.begin(), nums2.end(), nums1.begin() + m);
-	std::stable_sort(nums1.begin(), nums1.begin() + m+n);
-    }
-};
