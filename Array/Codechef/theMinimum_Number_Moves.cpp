b4ed3583254440514985cfcde92e83d7e5b60e9d
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-05-30 11:36:34 +0530

    1

--- Array/Codechef/theMinimum_Number_Moves.cpp
+++ Array/Codechef/theMinimum_Number_Moves.cpp
@@ -1,28 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long int
 
 int main()
 {
     ios_base::sync_with_stdio();
     cin.tie(NULL);
-    ll t;
-    cin>>t;
-    while(t--)
-    {
-        ll n,i,s=0,min=INT_MAX;
-        cin>>n;
-        ll a[n];
-        for(i=0;i<n;i++)
-        {
-            cin>>a[i];
-            s=s+a[i];
-            if(a[i]<=min)
-            {
-            min=a[i];
-            }
-        }
-        cout<<(s-n*min)<<"\n";
-    }
-    return 0;
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int n,opr=0;
+	cin>>n;
+
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+
+	sort(arr,arr+n);
+
+	for(int i=n-1;i>=1;i--)
+	{	
+	opr+=arr[i]-arr[0];
+	}
+	cout<<opr<<"\n";
+}
+    
 }
